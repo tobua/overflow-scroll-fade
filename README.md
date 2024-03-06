eff15df08b4d529e5c6012de5229999243e17ff3
Matthias Giger <i@matthiasgiger.com>
2024-03-06 22:49:11 +0100

    feat(package): initial setup for modern indicate alternative

---
+++ README.md
@@ -0,0 +1,20 @@
+<p align="center">
+  <img src="https://github.com/tobua/overflow-scroll-fade/raw/main/logo.png" alt="overflow-scroll-fade" width="300">
+</p>
+
+# overflow-scroll-fade
+
+Add a gradient-based dynamic fade effect to elements with scrollable overflow.
+
+## Usage
+
+```tsx
+import { Scroll } from 'overflow-scroll-fade'
+
+const MyScroll = () => (
+  <Scroll>
+    <p>overflow-scroll-fade</p>
+    <p>overflow-scroll-fade</p>
+  </Scroll>
+)
+```
---
+++ demo/index.tsx
@@ -0,0 +1,163 @@
+import { type CSSProperties } from 'react'
+import { createRoot } from 'react-dom/client'
+import { highlight } from 'sugar-high'
+import { scale } from 'optica'
+import { Scroll } from 'overflow-scroll-fade'
+import logo from './logo.png'
+
+document.body.style.display = 'flex'
+document.body.style.justifyContent = 'center'
+document.body.style.margin = '0'
+document.body.style.padding = '5vmin'
+
+const appStyles: CSSProperties = {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'normal',
+  width: '100%',
+  fontFamily: 'sans-serif',
+  maxWidth: 1000,
+  justifyContent: 'space-between',
+  gap: scale(20),
+}
+
+const Color = {
+  blue: {
+    ultralight: '#c3deff',
+    light: '#77b5ff',
+    regular: '#0075ff',
+    dark: '#0075ff',
+  },
+  boxes: ['#0075ff', '#ff002e', '#00ba6c', '#eb00ff', '#ffb800', '#09d3c7'],
+}
+
+const headingStyles = (as: 'h1' | 'h2' | 'h3'): CSSProperties => ({
+  margin: 0,
+  fontSize: as === 'h1' ? scale(36) : scale(24),
+})
+
+function Heading({
+  as = 'h1',
+  style,
+  ...props
+}: JSX.IntrinsicElements['h1' | 'h2' | 'h3'] & { as?: 'h1' | 'h2' | 'h3' }) {
+  const Component = as
+  return <Component {...props} style={{ ...headingStyles(as), ...style }} />
+}
+
+const paragraphStyles: CSSProperties = {
+  margin: 0,
+  fontSize: scale(18),
+}
+
+function Paragraph({ style, ...props }: JSX.IntrinsicElements['p']) {
+  return <p {...props} style={{ ...paragraphStyles, ...style }} />
+}
+
+function Code({ children }: { children: string }) {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        padding: scale(20),
+        background: Color.blue.ultralight,
+        borderRadius: scale(20),
+        fontFamily: 'monospace',
+      }}
+      dangerouslySetInnerHTML={{ __html: highlight(children) }}
+    />
+  )
+}
+
+let colorIndex = 0
+
+const boxStyles = (size: number, sizeFactor = 40): CSSProperties => {
+  const color = Color.boxes[colorIndex++ % 6]
+
+  return {
+    display: 'flex',
+    width: scale(100),
+    height: scale(size * sizeFactor),
+    background: color,
+    borderRadius: scale(10),
+  }
+}
+
+function Box({ size = 1 }) {
+  return <div style={boxStyles(size)} />
+}
+
+function App() {
+  return (
+    <div style={appStyles}>
+      <style>{`.sh__line {
+    display: block;
+    min-height: 20px;
+}
+
+:root {
+  --sh-class: #2d5e9d;
+  --sh-identifier: #354150;
+  --sh-sign: #8996a3;
+  --sh-property: #0550ae;
+  --sh-entity: #249a97;
+  --sh-jsxliterals: #6266d1;
+  --sh-string: #00a99a;
+  --sh-keyword: #f47067;
+  --sh-comment: #a19595;
+}`}</style>
+      <img
+        style={{ maxWidth: scale(400), width: '100%', alignSelf: 'center' }}
+        src={logo}
+        alt="masua Logo"
+      />
+      <Heading>overflow-scroll-fade</Heading>
+      <Paragraph>
+        Add a gradient-based dynamic fade effect to elements with scrollable overflow.
+      </Paragraph>
+      <Heading as="h2">Usage</Heading>
+      <Code>{`import { Fade } from 'overflow-scroll-fade'
+
+const MyGrid = () => (
+  <Fade>
+    <p>overflow-scroll-fade</p>
+    <p>overflow-scroll-fade</p>
+  </Fade>
+)`}</Code>
+      <Heading as="h2">Examples</Heading>
+      <Scroll style={{ maxWidth: 300 }} overflowStyle={{ gap: scale(20) }}>
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+      </Scroll>
+      <Heading as="h3">Vertical</Heading>
+      <Scroll
+        style={{ maxHeight: 200 }}
+        direction="vertical"
+        overflowStyle={{ gap: scale(20), flexDirection: 'column' }}
+      >
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+      </Scroll>
+      <Heading as="h3">Configuration</Heading>
+      <div style={{ display: 'grid', gridTemplateColumns: 'auto auto auto', gap: scale(10) }}>
+        <span>gutter</span>
+        <span>10</span>
+        <span>Space between elements.</span>
+        <span>gutter</span>
+        <span>10</span>
+        <span>Space between elements.</span>
+      </div>
+    </div>
+  )
+}
+
+createRoot(document.body as HTMLElement).render(<App />)
